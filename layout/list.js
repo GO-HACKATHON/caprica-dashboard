f7eda6977293aae70e00371448e802772d2c6616
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-03-25 22:46:42 +0700

    lint

--- layout/list.js
+++ layout/list.js
@@ -4,43 +4,28 @@ const List = ({accidents}) => {
   return (
     <div>
 
-    <main className="mw6 center">
-{
-  accidents.map(({user_id: userId, latitude, longitude, speed, created_at: time}, idx) => {
-    const hashed = hash(userId)
-    return (
-      <article key={idx} className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
-        <div className="dtc w2 w3-ns v-mid">
-          <img src={`http://mrmrs.io/photos/p/${hashed + 1}.jpg`} className="ba b--black-10 db br2 w2 w3-ns h2 h3-ns"/>
-        </div>
-        <div className="dtc v-mid pl3">
-          <h1 className="f6 f5-ns fw6 lh-title black mv0">Accident at {latitude}, {longitude}.</h1>
-          <h2 className="f6 fw4 mt0 mb0 black-60">User @{userId} with speed {speed}kph.  Time = {time}</h2>
-        </div>
-        <div className="dtc v-mid">
-          <form className="w-100 tr">
-            <button className="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60" type="submit">+ Send Help</button>
-          </form>
-        </div>
-      </article>
-    )
-  })
-}
-    {/* <article className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
-      <div className="dtc w2 w3-ns v-mid">
-        <img src="http://mrmrs.io/photos/p/3.jpg" className="ba b--black-10 db br2 w2 w3-ns h2 h3-ns"/>
-      </div>
-      <div className="dtc v-mid pl3">
-        <h1 className="f6 f5-ns fw6 lh-title black mv0">Arnoldo Degraff</h1>
-        <h2 className="f6 fw4 mt0 mb0 black-60">@99xx88randomhandle</h2>
-      </div>
-      <div className="dtc v-mid">
-        <form className="w-100 tr">
-          <button className="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60" type="submit">+ Follow</button>
-        </form>
-      </div>
-    </article> */}
-  </main>
+    {
+      accidents.map(({user_id: userId, latitude, longitude, speed, created_at: time}, idx) => {
+        const hashed = hash(userId)
+        return (
+          <article key={idx} className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
+            <div className="dtc w2 w3-ns v-mid">
+              <img src={`http://mrmrs.io/photos/p/${hashed + 1}.jpg`} className="ba b--black-10 db br2 w2 w3-ns h2 h3-ns"/>
+            </div>
+            <div className="dtc v-mid pl3">
+              <h1 className="f6 f5-ns fw6 lh-title black mv0">Accident at {latitude}, {longitude}.</h1>
+              <h2 className="f6 fw4 mt0 mb0 black-60">User @{userId} with speed {speed}kph.  Time = {time}</h2>
+            </div>
+            <div className="dtc v-mid">
+              <form className="w-100 tr">
+                <button className="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60" onClick={() => console.log(`Sending help to user ${userId}`)}>+ Send Help</button>
+              </form>
+            </div>
+          </article>
+        )
+      })
+    }
+
 </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,31 @@ import Head from 'next/head'
 import List from '../layout/list';
 
 export default withEvents(({accidents}) => (
-  <div>
+  <div className="main min-h-100">
     <Head>
       <meta httpEquiv='x-ua-compatible' content='ie=edge' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta charSet='utf-8' />
-        <link rel="stylesheet" href="/static/tachyons.min.css"/>
-        <title>Caprica Dashboard</title>
+      <link rel="stylesheet" href="/static/tachyons.min.css"/>
+      <title>Caprica Dashboard</title>
     </Head>
 
+    <main className="mw7 center black-80 ph4 pv5 min-h-100">
     <h1>Caprica Dashboard</h1>
+    <h2>Accidents</h2>
     <List accidents={accidents}/>
-    {/* <h2>Accidents</h2>
-    {
-      accidents.map(({user_id: userId, latitude, longitude, speed, created_at: time}, idx) => (
-        <div key={idx}>Accident happened: User {userId} - speed: {speed}, at {latitude}, {longitude}</div>
-      ))
-    } */}
+
+    <h2>Events</h2>
+
+    <h2>Helmet events</h2>
+  </main>
+  <style jsx>{
+    `
+    .main {
+      background-image: linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%);
+      min-height: 100vh;
+    }
+    `
+  }</style>
   </div>
 ))
